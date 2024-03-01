c88f5030d3238591a58574421e65525e46f2e3ae
Ross Light <ross@zombiezen.com>
2024-03-01 14:15:16 -0800

    Replace shell.nix with flake.nix

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  inputs = {
+    nixpkgs.url = "nixpkgs";
+    flake-utils.url = "flake-utils";
+  };
+
+  outputs = { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = [
+            pkgs.go_1_19
+          ];
+        };
+      }
+    );
+}
--- shell.nix
+++
@@ -1,6 +0,0 @@
-{ pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/998f0f7924198b2460458728de59fe738997f28e.tar.gz") {}
-}:
-
-pkgs.mkShell {
-  packages = [ pkgs.go_1_19 ];
-}
