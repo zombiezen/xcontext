34ab6080ee370ca38fceea709b521fbc37471899
Ross Light <ross@zombiezen.com>
2024-03-01 14:49:36 -0800

    Use Nix flake in GitHub Actions workflows

--- flake.nix
+++ flake.nix
@@ -8,12 +8,39 @@
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
+        go = pkgs.go_1_19;
       in
       {
         devShells.default = pkgs.mkShell {
           packages = [
-            pkgs.go_1_19
+            go
           ];
+        };
+
+        checks.go_test = pkgs.stdenv.mkDerivation {
+          name = "xcontext-go-test";
+          src = ./.;
+          __impure = true;
+
+          nativeBuildInputs = [
+            pkgs.cacert
+            go
+          ];
+
+          buildPhase = ''
+            runHook preBuild
+
+            HOME="$(mktemp -d)"
+            go test -mod=readonly -race -v ./...
+
+            runHook postBuild
+          '';
+
+          installPhase = ''
+            runHook preInstall
+            touch "$out"
+            runHook postInstall
+          '';
         };
       }
     );
